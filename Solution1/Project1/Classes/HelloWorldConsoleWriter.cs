e7a4789b38faca0f65daa68e6642bf1d2993f198
Tamir Levi <tamir@apiiro.com>
2024-11-21 16:43:23 +0200

    commit

---
+++ Solution1/Project1/Classes/HelloWorldConsoleWriter.cs
@@ -0,0 +1,9 @@
+namespace Project1.Classes;
+
+public class HelloWorldConsoleWriter
+{
+    public void WriteHelloWorld()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Solution1/Project1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Project1.Classes;
+
+new HelloWorldConsoleWriter().WriteHelloWorld();
